d9bd79eddc7254b84b8503b73bd99cd1fa5ee374
Sombrero1 <vovashevakrut@yandex.ru>
2025-10-01 23:07:26 +0300

    client amocrm and export script

--- backend/app/tools/__init__.py
+++ backend/app/tools/__init__.py
@@ -2,15 +2,18 @@
 Инструменты для агентов.
 """
 
+from .amocrm_tools import AMOCRM_TOOLS
 from .calc_tools import CALC_TOOLS
 from .fashn_tools import FASHN_TOOLS
 from .file_tools import FILE_TOOLS
 from .standard import STANDARD_TOOLS
 from .weather_tools import WEATHER_TOOLS
 from .voice_tools import VOICE_TOOLS
+from ..clients.amo_crm_integration import register_subdomain
 
 # Все доступные инструменты
 ALL_TOOLS = (
+    AMOCRM_TOOLS +
     CALC_TOOLS +
     FASHN_TOOLS +
     FILE_TOOLS +
@@ -20,11 +23,13 @@ ALL_TOOLS = (
 )
 
 __all__ = [
+    "AMOCRM_TOOLS",
     "CALC_TOOLS",
-    "FASHN_TOOLS", 
+    "FASHN_TOOLS",
     "FILE_TOOLS",
     "STANDARD_TOOLS",
     "WEATHER_TOOLS",
     "VOICE_TOOLS",
     "ALL_TOOLS",
+    "register_subdomain",
 ]
