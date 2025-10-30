856b4e60a0388ed8562960e2cf0309bd9d9fb739
Sombrero1 <vovashevakrut@yandex.ru>
2025-10-30 14:51:30 +0300

    фронт трейсов

--- migrations/langfuse/clickhouse/001_create_database.sql
+++
@@ -1,4 +0,0 @@
--- Миграция 001: Создание базы данных langfuse в ClickHouse
--- Выполняется при инициализации ClickHouse для Langfuse
-
-CREATE DATABASE IF NOT EXISTS langfuse;
--- migrations/langfuse/clickhouse/002_create_user.sql
+++
@@ -1,4 +0,0 @@
--- Миграция 002: Создание пользователя langfuse в ClickHouse
--- Выполняется после создания базы данных
-
-CREATE USER IF NOT EXISTS langfuse IDENTIFIED WITH sha256_password BY 'langfuse_password';
