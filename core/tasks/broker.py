ddfcb1dcb50cdacfa768ea1bac0117ec2c951907
viktor-shved <zambas124@gmail.com>
2025-12-04 12:29:42 +0300

    CRN BACK

--- core/tasks/broker.py
+++ core/tasks/broker.py
@@ -2,15 +2,17 @@
 TaskIQ брокер для всей системы.
 """
 
+import os
+
 from taskiq_redis import RedisStreamBroker, RedisAsyncResultBackend, ListRedisScheduleSource
 from taskiq import TaskiqScheduler
 
 from core.config.loader import load_merged_config
 from core.tasks.session_lock import session_lock_middleware
 
-# Загружаем конфиг напрямую, минуя get_settings() синглтон
+# Загружаем конфиг: env var имеет приоритет над conf.json
 _config = load_merged_config()
-_redis_url = _config.get("database", {}).get("redis_url", "redis://localhost:6379")
+_redis_url = os.getenv("DATABASE__REDIS_URL") or _config.get("database", {}).get("redis_url", "redis://localhost:6379")
 
 result_backend = RedisAsyncResultBackend(redis_url=_redis_url, result_ex_time=3600)
 broker = RedisStreamBroker(url=_redis_url).with_result_backend(result_backend).with_middlewares(session_lock_middleware)
